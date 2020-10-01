1b697751c1a622e00824e4fbe2feea0c757bc2e7
itsWagi <wzia.bscs16seecs@seecs.edu.pk>
2020-10-01 14:26:09 +0500

    Added Enter Functionality on Search and fixed up flex of events

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,18 +7,28 @@ class Search extends React.Component{
     this.state = {artist:''};
     this.handleSearchQuery = this.handleSearchQuery.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.pressEnter = this.pressEnter.bind(this)
   }
   handleSearchQuery(event){
-    this.setState({artist: event.target.value})
+    const artistName = event.target.value.replace(" ", "%20")
+    this.setState({artist: artistName})
   }
   handleSearch(event){
     this.props.searchApi(this.state.artist)
     event.preventDefault()
   }
+  pressEnter(event){
+    if(event.key === 'Enter'){
+      const artistName = event.target.value.replace(" ", "%20")
+      this.setState({artist: artistName})
+      this.props.searchApi(this.state.artist)
+      event.preventDefault()
+    }
+  }
   render(){
     return(
       <div className="searchcontainer">
-          <input placeholder="Search Artist..." onChange={this.handleSearchQuery} />
+          <input placeholder="Search Artist..." onChange={this.handleSearchQuery} onKeyUp={this.pressEnter}/>
           <div className="search-submit">
             <a onClick={this.handleSearch}>Search</a>
           </div>
