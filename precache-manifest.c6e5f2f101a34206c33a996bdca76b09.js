81bfd05d2b898beac305378bd43f737899d75c32
itsWagi <wzia.bscs16seecs@seecs.edu.pk>
2020-10-03 22:11:17 +0500

    Updates

---
+++ precache-manifest.c6e5f2f101a34206c33a996bdca76b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb2d4546d86db7ff1ecf3a2affa78f1",
+    "url": "/jobassignment/index.html"
+  },
+  {
+    "revision": "9bb220ec4306449b513f",
+    "url": "/jobassignment/static/css/main.5c2af996.chunk.css"
+  },
+  {
+    "revision": "83e3ac308cf81634149a",
+    "url": "/jobassignment/static/js/2.95166bd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jobassignment/static/js/2.95166bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb220ec4306449b513f",
+    "url": "/jobassignment/static/js/main.d51c99bc.chunk.js"
+  },
+  {
+    "revision": "1853746a9f7f20d4e072",
+    "url": "/jobassignment/static/js/runtime-main.5cf40c40.js"
+  }
+]);
